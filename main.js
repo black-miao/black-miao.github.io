51dbe31015a622529368e175403b4e2067b9a1b0
black_miao <992562557@qq.com>
2021-07-25 19:53:09 +0800

    加上点击效果

--- main.js
+++ main.js
@@ -9,15 +9,35 @@
 //     }, 100,img);
 // }
 let container = document.querySelector('.container');
+let browseImg = document.querySelector('.browseImg');
+let bimg = document.querySelector('.browseImg img');
 //add img
 add(21)
+
+function browse(src) {
+    browseImg.style.display = 'block';
+    browseImg.style.top = document.documentElement.scrollTop + 'px';
+    browseImg.addEventListener('click', () => back());
+    bimg.alt = src;
+    bimg.src = src;
+    document.body.style.overflow = 'hidden';
+}
+
+function back() {
+    browseImg.style.display = 'none';
+    bimg.alt = '';
+    bimg.src = '';
+    document.body.style.overflow = 'auto';
+}
+
 function add(num) {
     if (num != 0) {
         let item = document.createElement('div');
         let img = document.createElement('img');
         item.className = 'item';
         img.alt = '图片';
-        img.src = 'pic/pic ('+num+').jpg';
+        img.src = 'pic/pic (' + num + ').jpg';
+        img.addEventListener('click', (e) => {browse(e.target.src)});
         item.style.opacity = 0;
         item.appendChild(img);
         container.appendChild(item);
@@ -30,7 +50,8 @@ function add(num) {
         let img = document.createElement('img');
         item.className = 'item';
         img.alt = '图片';
-        img.src = 'pic/pic ('+num+').jpg';
+        img.src = 'pic/pic (' + num + ').jpg';
+        img.addEventListener('click', (e) => {browse(e.target.src)});
         item.style.opacity = 0;
         item.appendChild(img);
         container.appendChild(item);
